720f645566b739c34dd846cee7b880bf9ba32182
suchjose84 <suchjose@live.com>
2023-06-23 15:26:44 +0800

    fix connect to database, and swagger

---
+++ server/routes/users.js
@@ -0,0 +1,20 @@
+const routes = require('express').Router();
+const usersController = require('../controllers/usersController.js');
+// const { requiresAuth } = require('express-openid-connect');
+
+// GET all users
+routes.get('/users', usersController.getAllUsers);
+
+// GET a user
+routes.get('/users/:username', usersController.getUser);
+
+// POST create user
+routes.post('/users', usersController.addUser);
+
+// PUT update user
+routes.put('/users/:username', usersController.editUser);
+
+// DELETE a user
+routes.delete('/users/:username', usersController.deleteUser);
+
+module.exports = routes;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 // const {auth} = require('express-openid-connect');
 // const authConfig = require('./config/auth0.config.js');
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -11,10 +11,28 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
-// //connects to auth0
+//connects to auth0
 // app.use(auth(authConfig.config));
 
 // //connect to routes folder
 // app.use('/', require('./routes'));
 
+//connect to routes folder
+app.use('/', require('./routes'));
+
 //connect to db
+const db = require('./models');
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Connected to the database and server running on port ${port}.`);
+    });
+  })
+  .catch(err => {
+    console.log('Cannot connect to the database!', err);
+    process.exit();
+  });
