bc0206483461934f45c9aba93a65ab5aba34d87b
WillMarda <WillMarda@outlook.com>
2023-07-06 01:07:18 -0600

    update to styles_output

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,5 @@
-
 module.exports = {
-  content: ["./pokemon_index.html", "./eldenring_index.html", "./src/**/*.{html,js}"],
+  content: ["./pokemon_index.html", "./pokemon_script.js", "./eldenring_index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
