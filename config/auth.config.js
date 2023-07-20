bcf2988792b245b2abf748a61263a95c6eedb32e
WillMarda <WillMarda@outlook.com>
2023-07-19 22:34:28 -0600

    Update auth.config.js

--- config/auth.config.js
+++ config/auth.config.js
@@ -5,7 +5,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 module.exports = new GoogleStrategy({
   clientID: '278837145189-lp3a2s8bfldi3k770kof7mcq89n1lt9c.apps.googleusercontent.com',
   clientSecret: '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',
-  callbackURL: 'http://https://userwiki-oasd.onrender.com/auth/google/redirect',
+  callbackURL: 'https://userwiki-oasd.onrender.com/auth/google/redirect',
   passReqToCallback: true
 }, (req, accessToken, refreshToken, profile, done) => {
   // Implement your logic to handle the authenticated user profile
